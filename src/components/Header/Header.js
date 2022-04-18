581808ed7ffa589a588318fa30253a46b413fd06
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-18 18:18:14 +0600

    active route added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Header.css';
 import { signOut } from 'firebase/auth';
+import CustomLink from '../CustomLink/CustomLink';
 const Header = () => {
     const [user] = useAuthState(auth);
     return (
@@ -15,20 +16,20 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link as={Link} to='/home' href="#home">Home</Nav.Link>
-                            <Nav.Link as={Link} to='/blogs' href="#blogs">Blogs</Nav.Link>
-                            <Nav.Link as={Link} to='/about' href="#about">About</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/home' >Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/blogs' >Blogs</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/about' >About</Nav.Link>
                             {
                                 user ? 
-                                <Nav.Link onClick={()=>signOut(auth)} as={Link} to='/home' href="#login">Logout</Nav.Link>
+                                <Nav.Link as={CustomLink} onClick={()=>signOut(auth)} to='/home'>Logout</Nav.Link>
                                 :
-                                <Nav.Link as={Link} to='/login' href="#login">Login</Nav.Link>
+                                <Nav.Link as={CustomLink} to='/login'>Login</Nav.Link>
                             }
                             {
                                 user ?
                                 ""
                                 :
-                                <Nav.Link as={Link} to='/register' href="#register">Register</Nav.Link>
+                                <Nav.Link as={CustomLink} to='/register' href="#register">Register</Nav.Link>
                             }
                             
                         </Nav>
